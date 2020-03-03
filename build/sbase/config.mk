300df071bb3d671f982e4ba4936a7d9e7b39ec7a
Kiëd Llaentenn <kiedtl@protonmail.com>
2020-03-03 16:08:09 -0500

    busybox => sbase

---
+++ build/sbase/config.mk
@@ -0,0 +1,14 @@
+# sbase version
+VERSION = 0.0
+
+# paths
+PREFIX = /usr
+MANPREFIX = $(PREFIX)/share/man
+
+CC = cc
+AR = ar
+RANLIB = ranlib
+
+CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE=700 -D_FILE_OFFSET_BITS=64
+CFLAGS   = -std=c99 -Wall -pedantic -Os
+LDFLAGS  = -s
