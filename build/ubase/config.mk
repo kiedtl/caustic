760c5a9e4a96fd4cb2c80926420ece5d583be00a
Kiëd Llaentenn <kiedtl@protonmail.com>
2020-03-03 16:27:40 -0500

    ubase: add build dir

---
+++ build/ubase/config.mk
@@ -0,0 +1,15 @@
+# ubase version
+VERSION = 0.1
+
+# paths
+PREFIX = /usr
+MANPREFIX = $(PREFIX)/share/man
+
+CC = cc
+AR = ar
+RANLIB = ranlib
+
+CPPFLAGS = -D_FILE_OFFSET_BITS=64 -D_XOPEN_SOURCE=700 -D_GNU_SOURCE
+CFLAGS   = -std=c99 -Wall -Wextra -Os
+LDLIBS   = -lcrypt
+LDFLAGS  = -s
